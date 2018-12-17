c2c632a0ff73a11fff2282c1adc3a78c85c93167
andre odendaal <aodendaal.direct@gmail.com>
2018-12-16 16:21:23 -0800

    start datastructures and search - add timeit functions

--- recursion/recursion.py
+++ recursion/recursion.py
@@ -1,3 +1,4 @@
+
 # simple sum of numbers
 
 def listSum(numList):
@@ -42,3 +43,15 @@ def toString(n, base):
         return convertString[n]
     else:
         return toString(n // base, base) + convertString[n % base]
+
+
+
+if __name__ == '__main__':
+    import timeit
+
+    print(str(timeit.timeit("listSum([1, 3, 5, 7, 9])", setup="from __main__ import listSum"))+ " - listSum([1, 3, 5, 7, 9]")
+    print(timeit.timeit("listSum([1, 3, 5, 7, 9, 3, 5, 7, 9])", setup="from __main__ import listSum"))
+    print(timeit.timeit("listSum([1])", setup="from __main__ import listSum"))
+
+    print(listSum([1, 3, 5, 7, 9]))
+

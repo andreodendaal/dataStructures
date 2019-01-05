6bac5b24afe2f1992ba60f74c89e9d38fc816663
andre odendaal <aodendaal.direct@gmail.com>
2019-01-04 20:25:50 -0800

    start graphs, reorg folder structure to enable multiple technologies

---
+++ python/ds_graphs/graph_intro.py
@@ -0,0 +1,16 @@
+class Graph:
+    def __init__(self, vertices):
+        # Total number of vertices
+        self.vertices = vertices
+        self.array = []
+
+        for i in range(vertices):
+            temp = LinkedList()
+            self.array.append(temp)
+
+        ## 1/4/19 do lists and linkedlists first
+
+
+
+
+

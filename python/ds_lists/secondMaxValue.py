ef14f255f0a983974b85618cbc4328f33966f5dd
andre odendaal <aodendaal.direct@gmail.com>
2019-01-17 22:59:50 -0800

    initial findSecondMaximum commit

---
+++ python/ds_lists/secondMaxValue.py
@@ -0,0 +1,12 @@
+def findSecondMaximum(lst):
+    """
+    Given an array of size n, can you find the second maximum element in the list?
+    Implement your solution in Python and see if your output matches the correct output!
+
+    >>> findSecondMaximum([9,2,3,6])
+    6
+
+
+    :param lst:
+    :return:
+    """
